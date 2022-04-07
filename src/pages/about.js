a80a1b1d5e450397887691380be4731bf546269f
Pedro Carara <cararax@gmail.com>
2022-04-07 08:16:29 -0300

    home and about pages refactored to use layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,14 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
+    <Layout pageTitle="About me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
       <p>And i love my dear wife Heverlin hehehe</p>
-      <Link to="/">Back to Home</Link>
-    </main>
+    </Layout>
   )
 }
 
