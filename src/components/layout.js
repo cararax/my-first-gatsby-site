d90b09d7b45da600feff8b2081de4091b742616e
Pedro Carara <cararax@gmail.com>
2022-04-09 10:07:05 -0300

    Added query to pull blog posts

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,6 +33,11 @@ const Layout = ({ pageTitle, children }) => {
                     </li>
                     <li className={navLinkItem}>
                         <Link to="/about" className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>
+                            Blog
+                        </Link>
+                    </li>
                 </ul>
             </nav>
             <main>
